a1c3eb0ebbf217afbe8672e53813af826581dd45
DevPlus31 <b.mus@live.fr>
2018-11-05 01:14:29 +0100

    added AutorizeNetAccept Podfile

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -11,6 +11,17 @@ export class HelloWorldModel extends Observable {
     this.authorizeNet = new Authorizenet({apiLoginId: 'testing', clientKey: 'testKey'});
     let card= this.authorizeNet.createCardNumber({cardNumber: '1234567891234', cardExpirationMonth: '06', cardExpirationYear:  '21', ccvCode: '123'})
       console.log(card.getCardNumber())
+    //  console.log(card.validateCardData(this))
     console.log(this.authorizeNet)    // this.message = this.yourPlugin.message;
   }
+
+  OnValidationSuccessful() {
+
+  }
+
+  OnValidationFaield(object) {
+      console.log(object)
+  }
+
+
 }
