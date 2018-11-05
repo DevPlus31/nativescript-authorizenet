5f8fba9abcc3c0dd53f7d182df8e51b5547342ce
DevPlus31 <b.mus@live.fr>
2018-11-05 01:37:49 +0100

    starting iOS impl

--- src/authorizenet.ios.ts
+++ src/authorizenet.ios.ts
@@ -1,5 +1,19 @@
 import { Common } from './authorizenet.common';
+import * as app from "tns-core-modules/application";
+
 
 export class Authorizenet extends Common {
+
+    apiClient  = null
+    apiLoginId = null
+    clientKey  = null
+
+
+    constructor(props) {
+        super()
+
+        let handler = AcceptSDKHandler()
+    }
+
 
 }
